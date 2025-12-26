6c2f86d921bdc770fdef06d2332d7449b22431ca
Louis Lugas <louislugas@gmail.com>
2025-12-26 23:21:44 +0700

    update adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,31 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		output: {
+// 			bundleStrategy: 'inline'
+// 		},
+// 		router: {
+// 			type: 'hash'
+// 		},
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		})
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		output: {
-			bundleStrategy: 'inline'
-		},
-		router: {
-			type: 'hash'
-		},
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
